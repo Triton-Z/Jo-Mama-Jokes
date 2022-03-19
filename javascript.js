ec5220284d1a0211c4621e1010d64205f07d500a
Triton-Z <98340562+Triton-Z@users.noreply.github.com>
2022-03-18 23:30:18 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 function doritos(){
 var newWindow = window.open("https://triton-z.github.io/Tab-Generator/", "_blank", "top=100, left=100, width=800, height=500, menubar=yes,toolbar=yes, scrollbars=yes, resizable=yes");
 }
-for (var count = 0; count>68; count++;)
+for (var count = 0; count>68; count++)
 {
 doritos();
 }
